4e25d6ea648598052b2efc2d3cd775bf0ebfaec7
Sandip Nirmal <sandipnirmal@icloud.com>
2022-04-28 07:54:45 +0530

    ci: changes to fix gh-pages base path

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -17,7 +19,10 @@ const config = {
 			pages: 'build',
 			assets: 'build',
 			fallback: null,
-			precompress: false
+			precompress: false,
+			paths: {
+				base: dev ? '' : '/energy-rain-game'
+			}
 		}),
 
 		prerender: {
